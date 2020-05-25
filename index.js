9a0b8825123a7bf66b135f4a03dd5ac61de3c371
Adrian Kim <uknoiluv@gmail.com>
2020-05-24 18:55:45 -0700

    change js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var todoInput = document.getElementById('todo-input');
 var todoButton = document.getElementById('todo-button');
 var todoList = document.getElementById('todo-list');
 todoButton.addEventListener('click', function (e) {
-  var li = document.createElement('li');
-  li.innerHTML = todoInput.value;
-  li.addEventListener('click', function() {
-    li.remove();
-  });
-  todoList.appendChild(li);
+  if (todoInput.value && todoInput.value.length > 0) {
+    var li = document.createElement('li');
+    li.innerHTML = todoInput.value;
+    li.addEventListener('click', function() {
+      li.remove();
+    });
+    todoList.appendChild(li);
+    todoInput.value = '';
+  }
 });
